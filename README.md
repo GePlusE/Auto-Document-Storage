e96da01043af077233d6d49e921868e70f359d5e
GePlusE <eisenbeiner@me.com>
2021-02-20 19:36:55 +0100

    created detect_files()

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Auto-Document-Storage
 Automatically detects new PDFs in a specific directory, finds or creates the directory for the file, renames it and saves it to the target directory.
 
+### How to:
+- Use a prefix separated by "__" (two underscores) infront of a files name as the target directory's name (e.g., "FOLDER__Document Name.pdf"). Ths script will use the prefix automatically as the name of the target directory. 
+
 ### Format for credentials.py
 ```python
 directory_to_clean = "path/to/directory"
 target_directory = "path/to/target/directory"
+types_to_ignore = [".DS_Store"] # append list with your own types which should be ignored
 ```
--- Script.py
+++ Script.py
@@ -2,8 +2,24 @@ import os
 import credentials as creds
 from time import sleep
 
-directory_to_clean = creds.input_folder
-os.chdir(directory_to_clean)
-files = os.listdir()
+directory_to_clean = creds.directory_to_clean
+target_directory = creds.target_directory
+types_to_ignore = creds.types_to_ignore
 
-print(files)
+
+def detect_files(path=directory_to_clean):
+    os.chdir(path)
+    files = os.listdir()
+    # Create list of files considering the type_to_ignore from credentials
+    file_list = [f for f in files if os.path.splitext(f)[-1] not in types_to_ignore]
+    return file_list
+
+
+def rename_and_move(
+    origin_filename, origin_path=directory_to_clean, target_path=target_directory
+):
+
+    pass
+
+
+rename_files()
